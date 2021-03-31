bcfa6ec24341f5cb512cd58d21ca3d42a8a9148e
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-03-31 14:05:57 +0700

    func

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-with open('input.txt') as f_in:
-    d=''
-    for line in f_in:
-        r=line.strip()
-        d+=r+' '
+import re
+def start():
+    with open(input('Введите имя файла')) as f_in:
+        d = ''
+        for line in f_in:
+            r = line.strip()
+            d += r + ' '
+        return d
+'''#Функция убирает пробелы до знаков препинания (a строка на вход - d а выход)
+def whitespace(a):
+    a= a.split()
+    d = []
+    print(a)
+    for i in range(len(a)):
+
+        if a[i] in '.,!-?':
+            d[i-1] = d[i-1] + a[i]
+        else:
+            d.append(a[i])
+    d = ' '.join(map(str, d))
     print(d)
+    return d'''
 
-#Функция убирает пробелы до знаков препинания (a строка на вход - d а выход)
-a = input().split(' ')
-d = []
-print(a)
-for i in range(len(a)):
 
-    if a[i] in '.,!?':
-        d[i-1] = d[i-1] + a[i]
-    else:
-        d.append(a[i])
-d = ' '.join(map(str, d))
-print(d)
+def unic(a):
+    u = []
+    b = a.split()
+    for i in b:
+        if i not in u:
+            u.append(i)
+    print(u)
+    return u
 
-u=[]
-b=d.split()
-for i in b:
-    if i not in u:
-        u.append(i)
-print(u)
-z=[]
-w=[]
-for i in u:
-    if i[0].isupper():
-        z.append(i)
-    else:
-        w.append(i)
-print(z,w)
+
+def sort(b):
+    z = []
+    w = []
+    for i in b:
+        if i[0].isupper():
+            z.append(i)
+        else:
+            w.append(i)
+    print(z, w)
+    return z, w
+
+d=start()
+d=re.sub(r'\s+(?=(?:[,.?!:;…]))', r'', d)
+unic_words = unic(d)
+u, l = sort(unic_words)
