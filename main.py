a771c485c83fba0c0b22de8843371d842ad3b3c2
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-03-31 10:23:07 +0700

    test

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-#убирает пробелы перед знаком препинания
-#def dlt():
+with open('input.txt') as f_in:
+    d=''
+    for line in f_in:
+        r=line.strip()
+        d+=r+' '
+    print(d)
+
+#Функция убирает пробелы до знаков препинания (a строка на вход - d а выход)
 a = input().split(' ')
 d = []
 print(a)
@@ -11,3 +17,19 @@ for i in range(len(a)):
         d.append(a[i])
 d = ' '.join(map(str, d))
 print(d)
+
+
+u=[]
+b=d.split()
+for i in b:
+    if i not in u:
+        u.append(i)
+print(u)
+z=[]
+w=[]
+for i in u:
+    if i[0].isupper():
+        z.append(i)
+    else:
+        w.append(i)
+print(z,w)
